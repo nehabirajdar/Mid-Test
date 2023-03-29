2dd5f654e73289548ca07524289a6905339230d5
nehabirajdar <nsb9823@gmail.com>
2023-03-29 12:58:31 +0530

    Add project files.

---
+++ Mid Test/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mid_Test
+{
+    public class Program
+    {
+        private static double pi = 3.14;
+
+
+       
+        static void Main(string[] args)
+        {
+
+           
+            double r = Convert.ToDouble(Console.ReadLine());
+            double area = pi * r * r;
+            Console.WriteLine("Area of Circle" + area);
+
+
+        }
+    }
+}
